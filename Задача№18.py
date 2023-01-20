09f376ee3dae6782fb08a1cbc1ddf3a543583a89
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2023-01-20 21:12:21 +0300

    задачи решены2

--- Задача№18.py
+++ Задача№18.py
@@ -11,3 +11,24 @@ A = list(map(int, input("введите числа в массиве через 
 X = int(input("введите число: "))
 Z = [abs(A[i]-X) for i in range(len(A))]
 print(A[Z.index(min(Z))])
+
+""" или
+n = int(input())
+
+# list1 = list()
+# for i in range(n):
+#     list1.append(int(input()))
+
+list1 = [int(input()) for i in range(n)]
+k= int(input())
+
+m = abs(k - list1[0])
+number = list1[0]
+
+for i in range(1, len(list1)):
+    if m > abs(k - list1[i]):
+        m = abs(k - list1[i])
+        number = list1[i]
+
+print(number)
+"""
